f1697999fe03ad4e50371c3629ef0f6300631a54
Augusto Soares <soaresaugusto@edu.unifor.br>
2025-10-07 18:13:19 -0300

    initializing project

---
+++ src/main/java/com/ds6/Distrischool6Application.java
@@ -0,0 +1,13 @@
+package com.ds6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Distrischool6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Distrischool6Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ds6/Distrischool6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ds6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Distrischool6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
